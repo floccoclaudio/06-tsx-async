1e79a71a58621879361e7b83002cfd8a6c68988c
Claudio Flocco <flocco.claudio@yahoo.co.uk>
2021-07-21 18:53:38 +0200

    set up asyncthunk to recieve itemlist from fakestore api

--- src/features/mainSlice.ts
+++ src/features/mainSlice.ts
@@ -1,5 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit'
-
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { RootState } from '../app/store'
 
 interface ProductType {
     id: number;
@@ -23,12 +23,48 @@ const initialMainState: MainInitialStateType = {
     isSuccess: false,
 }
 
+export const fetchInventory = createAsyncThunk<
+    //return input type
+    ProductType[],
+    //output type
+    void,
+    //typing getState
+    { state: RootState; rejectValue: string }>(
+        'items/fetchFakeStoreItems',
+        async function fetchFakeStoreItems(_, { rejectWithValue }) {
+            try {
+                let response = await fetch('https://fakestoreapi.com/products/')
+                return await response.json()
+            }
+            catch (error) {
+                return rejectWithValue(error as string)
+            }
+
+        }
+    )
 
 export const mainSlice = createSlice({
     name: 'mainSlice',
     initialState: initialMainState,
     reducers: {
-    }
+
+    },
+    extraReducers:
+        builder => {
+            builder.addCase(fetchInventory.pending, state => {
+                state.isLoading = true
+            })
+            builder.addCase(fetchInventory.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.inventory = action.payload
+            })
+            builder.addCase(fetchInventory.rejected, state => {
+                state.isLoading = false;
+                state.isError = true;
+            })
+
+        }
 })
 
 export default mainSlice.reducer
